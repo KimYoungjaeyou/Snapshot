edcc9ff24a9693b697a36c7f21a87b132fac0509
KimYoungjaeyou <qoloa0326@naver.com>
2018-07-25 01:45:56 +0900

    Add files via upload

--- router/index.js
+++ router/index.js
@@ -5,8 +5,66 @@ var path = require('path')
 var main = require('./main/main')
 var trigger = require('./trigger/trigger')
 var monitoring = require('./monitoring/monitoring')
+var elasticsearch = require('elasticsearch');
+
+
+var client = elasticsearch.Client({
+  host: 'localhost:9200'
+});
+
+
+// client.indices.create({
+// index: 'test1'
+// },function(err,resp,status) {
+// if(err) {
+// 	console.log("Console already exists.");
+// }
+// else {
+// 	console.log("create",resp.index);
+// }
+// });
+client.search({
+index: 'basketball'
+},function(err,resp,status) {
+var hits = resp.hits.hits;
+
+if(err) {
+	console.log("Console already exists.");
+}
+else {
+	console.log(hits);
+}
+});
+
+client.ping({
+  requestTimeout: 30000,
+}, function (error) {
+  if (error) {
+    console.error('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});
+
+client.search({
+  q: 'basketball'
+}).then(function (body) {
+  var hits = body.hits.hits;
+}, function (error) {
+  if (error) {
+    console.error('elasticsearch cluster is down!');
+  } else {
+    console.log(hits);
+  }
+});
+
+
 
 //url routing
+client.search({
+	index: 'basketball'
+})
+
 router.get('/', function(req,res) {
 	res.render('main.ejs', {
     title : 'test'
--- router/main/main.js
+++ router/main/main.js
@@ -4,7 +4,6 @@ var router = express.Router()
 var path = require('path')
 
 
-//main page 는 login이 될 때만(즉 세션정보가 있을때만) 접근이 가능하게 하자.
 router.get('/', function(req,res) {
   res.render('main.ejs', {
     title : 'CLOUDZ 메인'
